1cac37401d4aedbcb7a5def992dd4b7286ebb95d
T <fortataata@gmail.com>
2023-10-21 19:08:11 +0200

    getting input value with useRef and with useState

--- 02-forms/src/components/SimpleInput.js
+++ 02-forms/src/components/SimpleInput.js
@@ -1,9 +1,42 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  // Two approaches to see every keystroke login (input value) on submit
+
+  // 1. Ref might be better if I am interested in the value once 
+  const nameInputRef = useRef();
+
+  // 2. State might be suitable for instant validation 
+  //    + if I want to reset the input value after the submit
+  const [enteredName, setEnteredName] = useState("");
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    // Ref for tracking every keystroke
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue + ", hello!");
+
+    // State for tracking every keystroke
+    event.preventDefault();
+    console.log(enteredName + ", hello!");
+
+    // Reset the value in the input on submit
+    setEnteredName("");
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
